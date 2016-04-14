97d68bbd78280eb74716f953ebb3d314f2676ce4
EGOIST <0x142857@gmail.com>
2016-04-14 10:23:27 +0800

    fix indent

--- test.js
+++ test.js
@@ -2,11 +2,11 @@ import test from 'ava'
 import saikou, {meme, h} from './'
 
 test('main', t => {
-	t.is(typeof saikou(), 'string')
-	t.true(Array.isArray(meme))
+  t.is(typeof saikou(), 'string')
+  t.true(Array.isArray(meme))
 })
 
 test('h', async t => {
-	const res = await h()
-	t.is(typeof res.hitokoto, 'string')
+  const res = await h()
+  t.is(typeof res.hitokoto, 'string')
 })
